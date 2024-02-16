5587e5e8fb9eee7104dbe4f6bb37f07b91903d74
Pratyaksh <pratyaksh0610@gmail.com>
2024-02-17 03:43:35 +0530

    zod types

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,33 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post("/todo", function (req, res) {});
+app.post("/todo", function (req, res) {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "Zod verification failed, wrong inputs",
+    });
+    return;
+  }
+});
 
 app.get("/todos", function (req, res) {});
-app.put("/completed", function (req, res) {});
+
+app.put("/completed", function (req, res) {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "Zod verification failed, wrong inputs",
+    });
+    return;
+  }
+});
 
 app.listen(3000, function () {
   console.log("Listening at " + port);
